4e59bcfadc787c444d7c24a6a77256d72b769739
Muhammad Awais <awaisrafique4929@gmail.com>
2025-10-24 12:07:40 +0500

    Add Swagger UI integration and update dependencies in package.json

--- RESTful-API-for-User-Details/app.js
+++ RESTful-API-for-User-Details/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 const userRouter = require('./User');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./api-docs/swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const LoggerMiddleware = function(req, res, next) {
     console.log(`Logger: ${req.method} ${req.url}`);
     next();
 }
+
+app.use('./api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(LoggerMiddleware);
 
